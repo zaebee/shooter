17d6d579677592d17b466fa7c6bbb3f2b1cb3518
andrei G <sinezub@yandex.ru>
2024-11-04 12:57:30 +0300

    Updated Player. Inherited from GameObject.

--- player.py
+++ player.py
@@ -1,40 +1,12 @@
-import os
-import pygame
+import objects
+import weapon
 
-class Fireball:
-    name = 'fireball'
-    
-    def __init__(self, filename=None, x=0, y=0):
-        """Initialize fireball image."""
-        self.x = x
-        self.y = y
-        self.name = filename or self.name
-        game_folder = os.path.dirname(__file__)
-        images_folder = os.path.join(game_folder, 'images')
-        # TODO: Add try/except block to catch FileNotFoundError
-        self.image = pygame.image.load(
-            os.path.join(images_folder, f'{self.name}.png'))
-        
-        def update(self):
-            pass
 
-class Player:
-
-    name = 'hero'
+class Player(objects.GameObject):
+    x = 0
+    y = 0
     speed = 5
-    x = 300
-    y = 500
-
-    def __init__(self, filename=None, x=0, y=0):
-        """Initialize hero image."""
-        self.x = x
-        self.y = y
-        self.name = filename or self.name
-        game_folder = os.path.dirname(__file__)
-        images_folder = os.path.join(game_folder, 'images')
-        # TODO: Add try/except block to catch FileNotFoundError
-        self.image = pygame.image.load(
-            os.path.join(images_folder, f'{self.name}.png'))
+    name = 'hero'
 
     def moveRight(self):
         self.x = self.x + self.speed
@@ -47,7 +19,9 @@ class Player:
 
     def moveDown(self):
         self.y = self.y + self.speed
-
-
-hero = Player()
-print(hero.image)
+        
+    def shoot(self):
+        item = weapon.Fireball(x=self.x, y=self.y)
+        # item.sound.set_volume(0.2)
+        # item.sound.play()
+        return item
