ab5ba80a085272c01594e46ec8111b9917f57fcb
andrei G <sinezub@yandex.ru>
2024-11-04 13:02:06 +0300

    Added animator

---
+++ animator.py
@@ -0,0 +1,19 @@
+import pygame
+
+def load_frames(filename: str, width: int, height: int):
+    """
+        Загружает спрайт с картинкой и возвращает 
+        список кадров для анимации.
+        Args:
+            filename - абсолютный путь до файла с кадрами.
+            width - ширина кадра
+            height - высота кадра
+    """
+   
+    sprite = pygame.image.load(filename).convert_alpha()
+    for y in range(0, sprite.get_height(), height):
+        for x in range(0, sprite.get_width(), width):
+            rect = pygame.Rect(x, y, width, height)
+            image = pygame.Surface(rect.size, pygame.SRCALPHA)
+            image.blit(sprite, (0, 0), rect)
+            yield image
